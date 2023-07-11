4e595a04e0ca8e83d5b989ab9ae8c5eb5da6301d
João Caio Pereira Melo <jcaio473@gmail.com>
2023-07-11 19:42:05 -0300

    Create Lab06_Vetor_05.c

---
+++ 6 - Vetor e Matriz/Lab06_Vetor_05.c
@@ -0,0 +1,26 @@
+/*5. Faça um vetor de tamanho 50 preenchido com o seguinte valor: (i+5*i)%(i+1), sendo i a posição do elemento no vetor. Em seguida imprima o vetor na tela.*/
+
+
+#include <stdio.h>
+#include <stdio.h>
+
+
+int main() {
+    int i;
+    int vetor[50];
+
+    for (i = 0; i < 50; i++)
+    {
+        vetor[i] = (i + 5 * i) % (i + 1);
+    }
+
+    printf("Vetor gerado:\n");
+    for (i = 0; i < 50; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+    
+    
+
+    return 0;
+}
